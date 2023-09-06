3f80e82b508e720ebde26a5d03aa92e3e26fdab7
SatireSage <sahajsingh2313@gmail.com>
2023-09-05 18:06:03 -0700

    Updates

---
+++ precache-manifest.996735062e71d8cc8a7f8ed4df0f4e34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "111f3672e30c191a12b6",
+    "url": "/terminal-portfolio/static/css/main.5ee06aec.chunk.css"
+  },
+  {
+    "revision": "111f3672e30c191a12b6",
+    "url": "/terminal-portfolio/static/js/main.111f3672.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/terminal-portfolio/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "cd07c54d866009c7aa5a",
+    "url": "/terminal-portfolio/static/css/2.9584202e.chunk.css"
+  },
+  {
+    "revision": "cd07c54d866009c7aa5a",
+    "url": "/terminal-portfolio/static/js/2.cd07c54d.chunk.js"
+  },
+  {
+    "revision": "2a3b53c7e53d6bd4337f",
+    "url": "/terminal-portfolio/static/js/runtime~main.2a3b53c7.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/terminal-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/terminal-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/terminal-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/terminal-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/terminal-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0d88eeb3132189da0982a5bc72bb20bc",
+    "url": "/terminal-portfolio/index.html"
+  }
+];
