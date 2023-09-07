8444d0b8dec59d191589d50a60232114929d4691
SatireSage <sahajsingh2313@gmail.com>
2023-09-06 23:25:05 -0700

    Updates

--- precache-manifest.a1d5c47e3838b5d2477290fcf780e7b1.js
+++ precache-manifest.a1d5c47e3838b5d2477290fcf780e7b1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "dfde648142ef67806984",
+    "revision": "d10a6d2de5f5f525ef58",
     "url": "/static/css/main.782c3353.chunk.css"
   },
   {
-    "revision": "dfde648142ef67806984",
-    "url": "/static/js/main.dfde6481.chunk.js"
+    "revision": "d10a6d2de5f5f525ef58",
+    "url": "/static/js/main.d10a6d2d.chunk.js"
   },
   {
     "revision": "9e0b21fe164afb870c62",
@@ -24,16 +24,12 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.e93d26b3.js"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -44,7 +40,11 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "e5216de7ad94ec0e34aa75b06c6b0d07",
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b8934a289964b166e9c81b56ed6ecdea",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49703b08012f12ff66a3f19482d2bed8.js"
+  "/precache-manifest.a1d5c47e3838b5d2477290fcf780e7b1.js"
 );
 
 workbox.clientsClaim();
